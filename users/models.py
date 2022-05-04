971c0205c59018c3731e3a0ee66309662140195e
lina1919 <lina1919@naver.com>
2022-05-04 19:55:37 +0900

    accounts model change

--- users/models.py
+++ users/models.py
@@ -13,10 +13,8 @@ class User(AbstractUser):
         (GENDER_FEMALE, "Female"),
         (GENDER_OTHER, "Other"),
     )
-    avatar = models.ImageField(blank=True)
     gender = models.CharField(choices=GENDER_CHOICES, max_length=10, blank=True)
     nickname = models.CharField(max_length=20, blank=True)
-    bio = models.TextField(default="", blank=True)
     birthdate = models.DateField(blank=True, null=True)
-    followers = models.ManyToManyField(settings.AUTH_USER_MODEL, symmetrical=False, related_name='follower')
-    followings = models.ManyToManyField(settings.AUTH_USER_MODEL, symmetrical=False, related_name='following')
+    email = models.EmailField(max_length=64,unique=True)
+    address = models.CharField(max_length=100, blank=True)
