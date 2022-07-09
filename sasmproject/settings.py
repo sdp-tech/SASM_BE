3a363479880963482f18a46e8c2acc6dbaf9af06
yxnxx <jh4rgh0@gmail.com>
2022-07-09 14:00:26 +0900

    settingdb

--- sasmproject/settings.py
+++ sasmproject/settings.py
@@ -112,8 +112,12 @@ WSGI_APPLICATION = 'sasmproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'sasm_db',
+        'USER': 'root',
+        'PASSWORD': 'mysql',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
