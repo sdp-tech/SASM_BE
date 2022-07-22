de3a0115f963be6cd825545dd5290a237978b810
yxnxx <jh4rgh0@gmail.com>
2022-07-23 01:14:00 +0900

    db 쌓기

--- places/urls.py
+++ places/urls.py
@@ -1,6 +1,7 @@
-from .views import place_like
+from .views import place_like, save_place_db
 from django.urls import path
 
 urlpatterns =[
     path('place_like/<int:id>/', place_like, name="place_like"),
+    path('save_place/', save_place_db),
 ]
--- places/views.py
+++ places/views.py
@@ -1,11 +1,18 @@
 from .models import Place
 from users.models import User
 
+from django.conf import settings
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
 from django.contrib.auth.decorators import login_required
 
+import json
+import requests
+import pandas as pd
+
 # Create your views here.
+
+rest_api_key = getattr(settings, 'KAKAO_REST_API_KEY')
 
 @login_required
 def place_like(request, id):
@@ -25,3 +32,35 @@ def place_like(request, id):
         place.place_like_cnt += 1
         place.save()
         return JsonResponse({'msg': 'click'})
+
+def addr_to_lat_lon(addr):
+    url = 'https://dapi.kakao.com/v2/local/search/address.json?query={address}'.format(address=addr)
+    headers = {"Authorization": "KakaoAK " + rest_api_key}
+    result = json.loads(str(requests.get(url, headers=headers).text))
+    match_first = result['documents'][0]['address']
+    return float(match_first['x']), float(match_first['y'])
+
+def save_place_db(request):
+    df = pd.read_excel("SASM_DB.xlsx", engine="openpyxl")
+    df = df.fillna('')
+    for dbfram in df.itertuples():
+        obj = Place.objects.create(
+            place_name=dbfram[1],
+            category=dbfram[2],
+            vegan_category=dbfram[3],
+            tumblur_category=dbfram[4],
+            reusable_con_category=dbfram[5],
+            pet_category=dbfram[6],
+            mon_hours=dbfram[7],
+            tues_hours=dbfram[8],
+            wed_hours=dbfram[9],
+            thurs_hours=dbfram[10],
+            fri_hours=dbfram[11],
+            sat_hours=dbfram[12],
+            sun_hours=dbfram[13],
+            place_review=dbfram[14],
+            address=dbfram[15],
+            short_cur=dbfram[16],
+        )
+        obj.save()
+    return JsonResponse({'msg': 'success'})
