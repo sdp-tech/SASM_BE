55975acd864ceb3c43585fb413f04f95e9566911
yxnxx <jh4rgh0@gmail.com>
2022-08-16 17:31:56 +0900

    cookie 발급 오류

--- stories/urls.py
+++ stories/urls.py
@@ -1,6 +1,7 @@
-from .views import story_like
+from .views import StoryDetailView, story_like
 from django.urls import path
 
 urlpatterns =[
     path('story_like/<int:id>/', story_like, name="story_like"),
+    path('story_detail/<int:id>', StoryDetailView.as_view(), name='story_detail'),
 ]
