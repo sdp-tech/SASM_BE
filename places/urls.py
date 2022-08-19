79965245471ae880656925151c25a9fa26557f57
lina1919 <lina1919@naver.com>
2022-08-19 22:50:13 +0900

    place detail view 추가

--- places/urls.py
+++ places/urls.py
@@ -1,7 +1,8 @@
-from .views import place_like, save_place_db
+from .views import PlaceDetailView, place_like, save_place_db
 from django.urls import path
 
 urlpatterns =[
     path('place_like/<int:id>/', place_like, name="place_like"),
     path('save_place/', save_place_db),
+    path('place_detail/<int:pk>/',PlaceDetailView.as_view(),name="place_detail"),
 ]
