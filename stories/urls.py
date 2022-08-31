4b5625e44c3526f21bb330339ac8441197c1c0e4
Yurim Park <63230463+lina1919@users.noreply.github.com>
2022-09-01 07:00:44 +0900

    feat : "add story likeview and listview"

--- stories/urls.py
+++ stories/urls.py
@@ -1,7 +1,8 @@
-from .views import StoryDetailView, story_like
+from .views import StoryDetailView, StoryLikeView, StoryListView
 from django.urls import path
 
 urlpatterns =[
-    path('story_like/<int:id>/', story_like, name="story_like"),
-    path('story_detail/<int:id>', StoryDetailView.as_view(), name='story_detail'),
+    path('story_like/', StoryLikeView.as_view({'post':'post'}), name="story_like"),
+    path('story_detail/<int:id>/', StoryDetailView.as_view(), name='story_detail'),
+    path('story_list/', StoryListView.as_view(), name='story_list'),
 ]
