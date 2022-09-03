a6836330c79725aede821996f1cc21b0afd17fd5
yxnxx <jh4rgh0@gmail.com>
2022-09-03 20:15:56 +0900

    feat : "Story pagination"

--- stories/urls.py
+++ stories/urls.py
@@ -4,5 +4,5 @@ from django.urls import path
 urlpatterns =[
     path('story_like/', StoryLikeView.as_view({'post':'post'}), name="story_like"),
     path('story_detail/<int:id>/', StoryDetailView.as_view(), name='story_detail'),
-    path('story_list/', StoryListView.as_view(), name='story_list'),
+    path('story_list/', StoryListView.as_view({'get': 'list'}), name='story_list'),
 ]
--- users/views/base_view.py
+++ users/views/base_view.py
@@ -38,7 +38,7 @@ class UserLikeView(viewsets.ModelViewSet):
             serializer = self.get_paginated_response(self.get_serializer(page, many=True,context={'request': request}).data) 
         else:
             serializer = self.get_serializer(page, many=True,context={'request': request})
-        return Response(data=serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 class SignupView(CreateAPIView):
     '''
