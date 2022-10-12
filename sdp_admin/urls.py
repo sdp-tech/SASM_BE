a64fa8912c6980718c55d87c2ef713ba50fb7003
Yurim Park <63230463+lina1919@users.noreply.github.com>
2022-10-13 07:34:22 +0900

    #150 fix : "change placephoto save code"

--- sdp_admin/urls.py
+++ sdp_admin/urls.py
@@ -1,16 +1,17 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views.stories_views import StoryViewSet
-from .views.places_views import PlaceViewSet, SNSTypeViewSet
+from .views.places_views import PlaceViewSet, SNSTypeViewSet, PlacesPhotoViewSet,SNSUrlViewSet
 
 app_name = 'sdp_admin'
 
 router = DefaultRouter()
 router.register(r'stories', StoryViewSet, basename="stories")
 router.register(r'places', PlaceViewSet, basename="places")
-
+router.register(r'snstypes', SNSTypeViewSet, basename="snstypes")
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('snstype/', SNSTypeViewSet.as_view({'get':'get'})),
+    path('placephoto/<int:pk>/',PlacesPhotoViewSet.as_view({'get':'get'}),name='placephoto'),
+    path('snsurl/<int:pk>/',SNSUrlViewSet.as_view({'get':'get'}),name='snsurl'),
 ]
