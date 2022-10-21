5b4425942bff6a84d46107af8caa886f32dbd0ac
Yurim Park <63230463+lina1919@users.noreply.github.com>
2022-10-22 00:07:07 +0900

    #172 fix : "delete pandas and save_place def"

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,7 @@ ENV PYTHONUNBUFFERED 1
 # pull official base image
 FROM python:3.10-alpine as builder
 # 의존성 패키지 설치 및 삭제
+RUN apk add py-mysqldb
 RUN apk update && apk add python3 python3-dev mariadb-dev build-base && pip3 install mysqlclient && apk del python3-dev mariadb-dev build-base
 RUN mkdir /srv/SASM_BE
 ADD . /srv/SASM_BE
--- places/views.py
+++ places/views.py
@@ -11,7 +11,7 @@ from rest_framework.response import Response
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.decorators import api_view
 from silk.profiling.profiler import silk_profile
-import pandas as pd
+# import pandas as pd
 import boto3
 from drf_yasg.utils import swagger_auto_schema
 from drf_yasg import openapi
@@ -217,74 +217,74 @@ def addr_to_lat_lon(addr):
     y=float(match_first['y'])
     return (x, y)
 
-@swagger_auto_schema(operation_id='func_places_save_place_get', method='get',responses={200:'success'},security=[])
-@api_view(['GET'])
-def save_place_db(request):
-    '''
-        SASM_DB 엑셀파일을 읽어 Place,PlacePhoto,SNS를 DB에 저장하는 함수
-    '''
-    df = pd.read_excel("SASM_DB.xlsx", engine="openpyxl")
-    df = df.fillna('')
-    for dbfram in df.itertuples():
-        place_name = dbfram[1]
-        ext = get_s3(place_name, "rep")
+# @swagger_auto_schema(operation_id='func_places_save_place_get', method='get',responses={200:'success'},security=[])
+# @api_view(['GET'])
+# def save_place_db(request):
+#     '''
+#         SASM_DB 엑셀파일을 읽어 Place,PlacePhoto,SNS를 DB에 저장하는 함수
+#     '''
+#     df = pd.read_excel("SASM_DB.xlsx", engine="openpyxl")
+#     df = df.fillna('')
+#     for dbfram in df.itertuples():
+#         place_name = dbfram[1]
+#         ext = get_s3(place_name, "rep")
         
-        obj = Place.objects.create(
-            place_name=dbfram[1],
-            category=dbfram[2],
-            vegan_category=dbfram[3],
-            tumblur_category=dbfram[4],
-            reusable_con_category=dbfram[5],
-            pet_category=dbfram[6],
-            mon_hours=dbfram[7],
-            tues_hours=dbfram[8],
-            wed_hours=dbfram[9],
-            thurs_hours=dbfram[10],
-            fri_hours=dbfram[11],
-            sat_hours=dbfram[12],
-            sun_hours=dbfram[13],
-            etc_hours=dbfram[14],
-            place_review=dbfram[15],
-            address=dbfram[16],
-            longitude=addr_to_lat_lon(dbfram[16])[0],
-            latitude=addr_to_lat_lon(dbfram[16])[1],
-            short_cur=dbfram[17],
-            phone_num=dbfram[18],
-            rep_pic = 'places/{}/rep.{}'.format(place_name, ext),
-            )
-        obj.save()
-        id = obj.id
-        for j in range(1,4):
-            ext = get_s3(place_name, str(j))            
-            img = PlacePhoto.objects.create(
-                image = 'places/{}/{}.{}'.format(place_name, str(j), ext),
-                place_id=id,
-                )
-            img.save()
+#         obj = Place.objects.create(
+#             place_name=dbfram[1],
+#             category=dbfram[2],
+#             vegan_category=dbfram[3],
+#             tumblur_category=dbfram[4],
+#             reusable_con_category=dbfram[5],
+#             pet_category=dbfram[6],
+#             mon_hours=dbfram[7],
+#             tues_hours=dbfram[8],
+#             wed_hours=dbfram[9],
+#             thurs_hours=dbfram[10],
+#             fri_hours=dbfram[11],
+#             sat_hours=dbfram[12],
+#             sun_hours=dbfram[13],
+#             etc_hours=dbfram[14],
+#             place_review=dbfram[15],
+#             address=dbfram[16],
+#             longitude=addr_to_lat_lon(dbfram[16])[0],
+#             latitude=addr_to_lat_lon(dbfram[16])[1],
+#             short_cur=dbfram[17],
+#             phone_num=dbfram[18],
+#             rep_pic = 'places/{}/rep.{}'.format(place_name, ext),
+#             )
+#         obj.save()
+#         id = obj.id
+#         for j in range(1,4):
+#             ext = get_s3(place_name, str(j))            
+#             img = PlacePhoto.objects.create(
+#                 image = 'places/{}/{}.{}'.format(place_name, str(j), ext),
+#                 place_id=id,
+#                 )
+#             img.save()
         
-        k = 19
-        while(True):
-            if(k<25 and len(dbfram[k])!=0):
-                sns_type = dbfram[k]
-                if SNSType.objects.filter(name=sns_type).exists():
-                    obj1 = SNSUrl.objects.create(
-                        snstype_id=SNSType.objects.get(name=sns_type).id,
-                        url = dbfram[k+1],
-                        place_id=id,
-                    )
-                    obj1.save()
-                else:
-                    obj2 = SNSType.objects.create(
-                        name = sns_type,
-                    )
-                    obj2.save()
-                    obj3 = SNSUrl.objects.create(
-                        snstype_id=obj2.id,
-                        url = dbfram[k+1],
-                        place_id=id,
-                    )
-                    obj3.save()
-                k+=2
-            else:
-                break
-    return Response({'msg': 'success'})
+#         k = 19
+#         while(True):
+#             if(k<25 and len(dbfram[k])!=0):
+#                 sns_type = dbfram[k]
+#                 if SNSType.objects.filter(name=sns_type).exists():
+#                     obj1 = SNSUrl.objects.create(
+#                         snstype_id=SNSType.objects.get(name=sns_type).id,
+#                         url = dbfram[k+1],
+#                         place_id=id,
+#                     )
+#                     obj1.save()
+#                 else:
+#                     obj2 = SNSType.objects.create(
+#                         name = sns_type,
+#                     )
+#                     obj2.save()
+#                     obj3 = SNSUrl.objects.create(
+#                         snstype_id=obj2.id,
+#                         url = dbfram[k+1],
+#                         place_id=id,
+#                     )
+#                     obj3.save()
+#                 k+=2
+#             else:
+#                 break
+#     return Response({'msg': 'success'})
