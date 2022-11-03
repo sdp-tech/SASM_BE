3f5c351debd914a2b05058555e43afe088ff188c
Yurim Park <63230463+lina1919@users.noreply.github.com>
2022-11-03 21:08:45 +0900

    #177 fix : "change to email host to sdp gmail account"

--- sasmproject/settings/base.py
+++ sasmproject/settings/base.py
@@ -180,9 +180,11 @@ SIMPLE_JWT = {
 
 #이메일 인증
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.daum.net'
-EMAIL_PORT = 465
-EMAIL_USE_SSL = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = secrets['EMAIL_HOST_USER']
+EMAIL_HOST_PASSWORD = secrets['EMAIL_HOST_PASSWORD']
+EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = secrets['EMAIL_HOST_USER']
 
 #corheaders
