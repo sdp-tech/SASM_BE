c60fd671732f31de0b29078de436ba444b101613
Yurim Park <63230463+lina1919@users.noreply.github.com>
2022-11-30 20:32:41 +0900

    #214 fix : "change settings.py simple_jwt and rest_framework"

--- sasmproject/settings/local.py
+++ sasmproject/settings/local.py
@@ -13,4 +13,7 @@ ALLOWED_HOSTS = ['0.0.0.0','127.0.0.1','localhost']
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=28),
+    'ROTATE_REFRESH_TOKENS': False,  # true면 토큰 갱신 시 refresh도 같이 갱신
+    'BLACKLIST_AFTER_ROTATION': True,
 }
