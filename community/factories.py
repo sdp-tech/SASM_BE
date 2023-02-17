22356dd173f1aaca3fc9f6558d6a1cb1ce2645e5
yew00n <yewoon.dev@gmail.com>
2023-02-18 00:58:24 +0900

    #298 test : import factory_boy and faker

---
+++ community/factories.py
@@ -0,0 +1,109 @@
+from users.models import User
+from community.models import Board, Post, PostComment, PostContentStyle, PostHashtag, PostPhoto, PostLike, PostComment, PostCommentPhoto, PostReport, PostCommentReport
+
+import factory
+import factory.fuzzy
+from factory.django import DjangoModelFactory
+
+from faker import Faker
+
+
+fake = Faker()
+
+class UserFactory(DjangoModelFactory):
+    class Meta:
+        model = 'users.User'
+    
+    nickname = fake.pystr(max_chars=20)
+    email = factory.Sequence(lambda n: "user%s@test.com" % n)
+    password = factory.Faker('word')
+    is_staff = True
+    is_admin = True
+    is_superuser = True
+    is_sdp =True
+
+
+class PostContentStyle(DjangoModelFactory):
+    class Meta:
+        model = 'community.PostContentStyle'
+
+    name = factory.Faker('word')
+    styled_content = factory.Faker('sentence')
+
+
+class BoardFactory(DjangoModelFactory):
+    class Meta:
+        model = 'community.Board'
+
+    name = factory.Faker('word')
+    supports_hashtags = factory.fuzzy.FuzzyChoice([True, False])
+    supports_post_photos = factory.fuzzy.FuzzyChoice([True, False])
+    supports_post_comment_photos = factory.fuzzy.FuzzyChoice([True, False])
+    # supports_post_comments = factory.fuzzy.FuzzyChoice([True, False])
+    supports_post_comments = True
+    post_content_style = factory.SubFactory(PostContentStyle)
+
+
+class PostFactory(DjangoModelFactory):
+    class Meta:
+        model = Post
+
+    title = factory.Faker('word')  
+    content = factory.Faker('sentence')
+    board = factory.SubFactory(BoardFactory)
+    writer = factory.SubFactory(UserFactory)
+
+
+class PostHashtagFactory(DjangoModelFactory):
+    class Meta:
+        model = PostHashtag
+
+    name = factory.Faker('word')
+    post = factory.SubFactory(PostFactory)
+
+
+class PostPhotoFactory(DjangoModelFactory):
+    class Meta:
+        model = PostPhoto
+
+    # ref. https://stackoverflow.com/questions/25806428/how-to-make-factoryboys-imagefield-generate-image-before-save-is-called
+    image = factory.django.ImageField(width=1024, height=768)
+    post = factory.SubFactory(PostFactory)
+
+
+class PostCommentFactory(DjangoModelFactory):
+    class Meta:
+        model = PostComment
+    
+    post = factory.SubFactory(PostFactory)
+    content = factory.Faker('sentence')
+    isParent = factory.fuzzy.FuzzyChoice([True, False])
+
+    # self FK
+    # ref. https://github.com/FactoryBoy/factory_boy/issues/173
+    parent = factory.LazyAttribute(lambda x: PostCommentFactory(parent=None))
+    writer = factory.SubFactory(UserFactory)
+    mention = factory.SubFactory(UserFactory)
+
+
+class PostReportFactory(DjangoModelFactory):
+    category = factory.fuzzy.FuzzyChoice(choices=[
+            "게시판 성격에 부적절함",
+            "음란물/불건전한 만남 및 대화",
+            "사칭/사기성 게시글",
+            "욕설/비하",
+            "낚시/도배성 게시글",
+            "상업적 광고 및 판매"])
+    post = factory.SubFactory(PostFactory)
+    reporter = factory.SubFactory(UserFactory)
+
+
+class PostCommentReportFactory(DjangoModelFactory):
+    category = factory.fuzzy.FuzzyChoice(choices=[
+            "음란물/불건전한 만남 및 대화",
+            "사칭/사기성 댓글",
+            "욕설/비하",
+            "낚시/도배성 댓글",
+            "상업적 광고 및 판매"])
+    post = factory.SubFactory(PostCommentFactory)
+    reporter = factory.SubFactory(UserFactory)
--- community/tests/test_views.py
+++ community/tests/test_views.py
@@ -8,6 +8,7 @@ from rest_framework import status
 
 from users.models import User
 from community.models import Board, Post, PostComment
+from community.factories import BoardFactory, PostFactory, PostCommentFactory
 
 
 class PostApiTests(TestCase):
@@ -15,85 +16,114 @@ class PostApiTests(TestCase):
 
     @classmethod
     def setUpTestData(cls):
-        
+
+        # cls.test_user = UserFactory()
         cls.test_user = User.objects.create_superuser(
-            email='test@test.test',
+            email='test@test.com',
             nickname='test',
             password='test')
-
-        cls.board = Board.objects.create(
-            name="test board",
-            supports_hashtags=True,
-            supports_post_photos=True,
-            supports_post_comment_photos=True,
-            supports_post_comments=True)
-
-        # cls.board2 = Board.objects.create(
-        #     name="test board2",
-        #     supports_hashtags=False,
-        #     supports_post_photos=False,
-        #     supports_post_comment_photos=False,
-        #     supports_post_comments=False)
-
-        cls.post = Post.objects.create(
-            title="test post",
-            content="this is a test post",
-            board=cls.board,
-            writer=cls.test_user)
+        
+        cls.board = BoardFactory()
+        cls.post = PostFactory(writer=cls.test_user)
 
 
     def test_post_create_api(self):
-
+        obj_cnt_before = Post.objects.count()
+        
         path = reverse('post_create')
         data = {
             'board': 1,
             'title': 'Test Title',
             'content': 'Test Content'
         }
-
         self.client.force_login(user=self.test_user)
         response = self.client.post(path, data)
+        
+        obj_cnt_after = Post.objects.count()
 
-        # 응답코드 확인
+
+        # 응답코드
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        # 입력내용과 DB 저장내용의 일치 확인
-        created_post = Post.objects.get(title='Test Title')
-        self.assertEqual(created_post.content, 'Test Content')
 
+        # 생성
+        self.assertEqual(obj_cnt_after, obj_cnt_before + 1)
+
+        # 필요한 필드 포함
+        created_post = Post.objects.order_by('-created').first()
+        created_post_qs = Post.objects.filter(id=created_post.id)
+        created_post_field_list = list(created_post_qs.values()[0].keys())
+        required_fields = ['title',
+                           'content',
+                           'board_id',
+                           'writer_id',
+                           'like_cnt',
+                           'view_cnt',
+                           'comment_cnt']
+
+        for field in required_fields:
+            self.assertTrue(field in created_post_field_list)
+
+        # 내용 일치
         selected_board = Board.objects.get(id=1)
         self.assertEqual(created_post.board, selected_board)
+        self.assertEqual(created_post.title , 'Test Title')
+        self.assertEqual(created_post.content, 'Test Content')
 
 
     def test_post_update_api(self):
+        obj_cnt_before = Post.objects.count()
 
         path = reverse('post_update', kwargs={'post_id':1})
         data = {
             'title': 'Test Update Title',
             'content': 'Test Update Content'
         }
-
         self.client.force_login(user=self.test_user) # isWriter
         response = self.client.put(path, data, content_type='application/json')
 
-        # 응답코드 확인
+        obj_cnt_after = Post.objects.count()
+
+        # 응답코드
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        # 입력내용과 DB 저장내용의 일치 확인
-        updated_post = Post.objects.get(title='Test Update Title')
+
+        # row count 유지
+        self.assertEqual(obj_cnt_after, obj_cnt_before)
+
+        # 필요한 필드 포함
+        updated_post = Post.objects.order_by('-updated').first()
+        updated_post_qs = Post.objects.filter(id=updated_post.id)
+        updated_post_field_list = list(updated_post_qs.values()[0].keys())
+        required_fields = ['title',
+                           'content',
+                           'board_id',
+                           'writer_id',
+                           'like_cnt',
+                           'view_cnt',
+                           'comment_cnt']
+
+        for field in required_fields:
+            self.assertTrue(field in updated_post_field_list)
+
+        # 내용 일치
+        self.assertEqual(updated_post.title, 'Test Update Title')
         self.assertEqual(updated_post.content, 'Test Update Content')
 
 
     def test_post_delete_api(self):
 
-        path = reverse('post_delete', kwargs={'post_id':1})
+        obj_cnt_before = Post.objects.count()
 
+        path = reverse('post_delete', kwargs={'post_id':1})
         self.client.force_login(user=self.test_user) # isWriter
         response = self.client.delete(path, content_type='application/json')
 
-        # 응답코드 확인
+        obj_cnt_after = Post.objects.count()
+
+        # 응답코드
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        # DB에서 post 개수 0 확인
-        posts_cnt = Post.objects.count()
-        self.assertEqual(posts_cnt, 0)        
+
+        # 삭제
+        self.assertEqual(obj_cnt_after, obj_cnt_before - 1)
 
 
     def test_post_list_api(self):
@@ -103,29 +133,36 @@ class PostApiTests(TestCase):
             'board': 1,
             'latest': True
         }
-
+        selected_board = Board.objects.get(id=1)
+        PostFactory.create_batch(5, board=selected_board) # board 1에 post 5개 생성
         self.client.force_login(user=self.test_user)
         response = self.client.get(path, data)
 
-        # 응답코드 확인
+        # 응답코드
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        # DB에서 post 개수와 일치 확인
-        posts_cnt = Post.objects.count()
+
+        # 조회 건수와 DB post 개수 일치 확인
+        posts_cnt = Post.objects.filter(board=selected_board).count()
         self.assertEqual(response.data['data']['count'], posts_cnt)
 
 
     def test_post_detail_api(self):
 
         path = reverse('post_detail', kwargs={'post_id':1})
-
         self.client.force_login(user=self.test_user)
         response = self.client.get(path)
 
-        # 응답코드 확인
+        # 응답코드
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        # 해당 post와 정보일치 확인
-        selected_post = Post.objects.get(id=1)
-        self.assertEqual(response.data['content'], selected_post.content)
+
+        # 내용 일치
+        post = Post.objects.get(id=1)
+        self.assertEqual(response.data['title'], post.title)
+        self.assertEqual(response.data['content'], post.content)
+        self.assertEqual(response.data['board'], post.board_id)
+        self.assertEqual(User.objects.get(email=response.data['email']).id, post.writer_id)
+        self.assertEqual(response.data['likeCount'], post.like_cnt)
+        self.assertEqual(response.data['viewCount'], post.view_cnt)
 
 
 class PostCommentListApiTests(TestCase):
@@ -133,42 +170,38 @@ class PostCommentListApiTests(TestCase):
 
     @classmethod
     def setUpTestData(cls):
-        
+
+        # cls.test_user = UserFactory()
         cls.test_user = User.objects.create_superuser(
-            email='test@test.test',
+            email='test@test.com',
             nickname='test',
             password='test')
-
-        cls.board = Board.objects.create(
-            name="test board",
-            supports_hashtags=True,
-            supports_post_photos=True,
-            supports_post_comment_photos=True,
-            supports_post_comments=True)
-
-        cls.post = Post.objects.create(
-            title="test post",
-            content="this is a test post",
-            board=cls.board)
-
-        cls.post_comment = PostComment.objects.create(
-            post=cls.post,
-            content="this is a test post comment",
-            isParent=True,
-            writer=cls.test_user)
+        
+        cls.board = BoardFactory()
+        cls.post = PostFactory(writer=cls.test_user)
+        cls.post_comment = PostCommentFactory(writer=cls.test_user)
 
 
     def test_post_comment_get_api(self):
 
+        selected_post = Post.objects.get(id=1)
+        PostCommentFactory.create_batch(5, post=selected_post) # post 1에 comment 5개 생성
+
         path = reverse('post_comment_list')
         self.client.force_login(user=self.test_user)
         response = self.client.get(path+'?post=1')
-        
-        # 응답코드 확인
+
+        # 응답코드
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        # 조회 건수와 DB post 개수 일치 확인
+        post_comments_cnt = PostComment.objects.filter(post=selected_post).count()
+        self.assertEqual(response.data['data']['count'], post_comments_cnt)
 
 
     def test_post_comment_create_api(self):
+
+        obj_cnt_before = PostComment.objects.count()
 
         path = reverse('post_comment_create')
         data = {
@@ -176,46 +209,85 @@ class PostCommentListApiTests(TestCase):
             'content': 'Test Content',
             'isParent': True
         }
-
         self.client.force_login(user=self.test_user)
         response = self.client.post(path, data)
+        obj_cnt_after = PostComment.objects.count()
 
-        # 응답코드 확인
+        # 응답코드
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        # 입력내용과 DB 저장내용의 일치 확인
-        created_post_comment = PostComment.objects.get(id=2)
-        self.assertEqual(created_post_comment.content, 'Test Content')
 
-        selected_post = Post.objects.get(id=1)
-        self.assertEqual(created_post_comment.post, selected_post)     
+        # 생성
+        self.assertEqual(obj_cnt_after, obj_cnt_before + 1)
+
+        # 필요한 필드 포함
+        created_post_comment = PostComment.objects.order_by('-created').first()
+        created_post_comment_qs = PostComment.objects.filter(id=created_post_comment.id)
+        created_post_comment_field_list = list(created_post_comment_qs.values()[0].keys())
+        required_fields = ['post_id',
+                           'content',
+                           'isParent',
+                           'parent_id',
+                           'writer_id']
+
+        for field in required_fields:
+            self.assertTrue(field in created_post_comment_field_list)
+
+        # 내용 일치
+        post = Post.objects.get(id=1)
+        self.assertEqual(created_post_comment.post, post)
+        self.assertEqual(created_post_comment.content, 'Test Content')
+        self.assertEqual(created_post_comment.isParent, True)
 
 
     def test_post_comment_update_api(self):
 
-        path = reverse('post_comment_update', kwargs={'post_comment_id':1})
+        obj_cnt_before = PostComment.objects.count()
+
+        path = reverse('post_comment_update', kwargs={'post_comment_id':2})
         data = {
             'content': 'Test Update Content'
         }
-
         self.client.force_login(user=self.test_user) # isWriter
         response = self.client.put(path, data, content_type='application/json')
 
+        obj_cnt_after = PostComment.objects.count()
+
+
         # 응답코드 확인
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        # 입력내용과 DB 저장내용의 일치 확인
-        updated_post_comment = PostComment.objects.get(id=1)
+
+        # row count 유지
+        self.assertEqual(obj_cnt_after, obj_cnt_before)
+
+        # 필요한 필드 포함
+        updated_post_comment = PostComment.objects.order_by('-updated').first()
+        updated_post_comment_qs = PostComment.objects.filter(id=updated_post_comment.id)
+        updated_post_comment_field_list = list(updated_post_comment_qs.values()[0].keys())
+        required_fields = ['post_id',
+                           'content',
+                           'isParent',
+                           'parent_id',
+                           'writer_id']
+
+        for field in required_fields:
+            self.assertTrue(field in updated_post_comment_field_list)
+
+        # 내용 일치
         self.assertEqual(updated_post_comment.content, 'Test Update Content')
 
 
     def test_post_comment_delete_api(self):
 
-        path = reverse('post_comment_delete', kwargs={'post_comment_id':1})
+        obj_cnt_before = PostComment.objects.count()
 
+        path = reverse('post_comment_delete', kwargs={'post_comment_id':2})
         self.client.force_login(user=self.test_user) # isWriter
         response = self.client.delete(path, content_type='application/json')
 
+        obj_cnt_after = PostComment.objects.count()
+
         # 응답코드 확인
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        # DB에서 post 개수 0 확인
-        post_comments_cnt = PostComment.objects.count()
-        self.assertEqual(post_comments_cnt, 0)   
+
+        # 삭제
+        self.assertEqual(obj_cnt_after, obj_cnt_before - 1)
