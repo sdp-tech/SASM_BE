f6dec38174fea6484005fd61c8be1eb2f21a7b16
Sejong Kim <sepaper@naver.com>
2023-02-26 21:05:28 +0900

    #310 fix : raise error for incorrect password
    Signed-off-by: Sejong Kim <sepaper@naver.com>

--- users/services.py
+++ users/services.py
@@ -19,15 +19,16 @@ from users.selectors import UserSelector
 JWT_PAYLOAD_HANDLER = api_settings.JWT_PAYLOAD_HANDLER
 JWT_ENCODE_HANDLER = api_settings.JWT_ENCODE_HANDLER
 
+
 class UserService:
     def __init__(self):
         pass
     # def __init__(self, user: User):
     #     self.user = user
-    
+
     # def update(self, profile_image: str, password: str, nickname: str, address: str, birthdate: str, gender: str) -> User:
     #     user = User.objects.get(id=self.user.id)
-        
+
     #     user.update_profile_image(profile_image)
 
     #     user.full_clean()
@@ -40,9 +41,9 @@ class UserService:
 
         user = user_selector.get_user_from_email(email)
 
-        if not user_selector.check_password(user,password):
-            exceptions.ValidationError({"error": "비밀번호가 틀립니다."})
-        
+        if not user_selector.check_password(user, password):
+            raise exceptions.ValidationError({"error": "비밀번호가 틀립니다."})
+
         token = RefreshToken.for_user(user=user)
         data = {
             'email': user.email,
@@ -59,36 +60,37 @@ class UserService:
 
         except TokenError:
             raise InvalidToken()
-        
+
     def check_email(self, email: str):
         user_selector = UserSelector()
 
         check_email = user_selector.check_email(email)
 
-        if(check_email):
+        if (check_email):
             return '존재하는 이메일입니다'
-        
+
         return '존재하지 않는 이메일입니다'
-    
+
     def check_rep(self, type: str, value: str):
         user_selector = UserSelector()
 
-        if(type=='email'):
+        if (type == 'email'):
             check_email = user_selector.check_email(value)
-            if(check_email):
+            if (check_email):
                 return '이미 사용중인 이메일입니다'
             return '사용 가능한 이메일입니다'
-        
-        if(type=='nickname'):
+
+        if (type == 'nickname'):
             check_nickname = user_selector.check_nickname(value)
-            if(check_nickname):
+            if (check_nickname):
                 return '이미 사용중인 닉네임입니다'
             return '사용 가능한 닉네임입니다'
+
 
 class PasswordResetService:
     def __init__(self):
         pass
-    
+
     def email_auth_string():
         LENGTH = 5
         string_pool = string.ascii_letters + string.digits
@@ -101,18 +103,18 @@ class PasswordResetService:
         user_selector = UserSelector()
 
         user = user_selector.get_user_from_email(email)
-        
+
         payload = JWT_PAYLOAD_HANDLER(user)
         jwt_token = JWT_ENCODE_HANDLER(payload)
         code = PasswordResetService.email_auth_string()
 
         html_content = render_to_string('users/password_reset.html', {
-                    'user': user,
-                    'nickname' : user.nickname,
-                    'uid': force_str(urlsafe_base64_encode(force_bytes(user.pk))),
-                    'token': jwt_token,
-                    'code': code,
-                })
+            'user': user,
+            'nickname': user.nickname,
+            'uid': force_str(urlsafe_base64_encode(force_bytes(user.pk))),
+            'token': jwt_token,
+            'code': code,
+        })
 
         user.code = code
         user.save()
@@ -120,16 +122,18 @@ class PasswordResetService:
         mail_subject = '[SDP] 비밀번호 변경 메일입니다'
         to_email = user.email
         from_email = 'sdpygl@gmail.com'
-        msg = EmailMultiAlternatives(mail_subject,'...',from_email, [to_email])
+        msg = EmailMultiAlternatives(
+            mail_subject, '...', from_email, [to_email])
         msg.attach_alternative(html_content, "text/html")
-        file_path = os.path.join(settings.BASE_DIR, 'static/img/SASM_LOGO_BLACK.png')
-        img_data = open(file_path,'rb').read()
+        file_path = os.path.join(
+            settings.BASE_DIR, 'static/img/SASM_LOGO_BLACK.png')
+        img_data = open(file_path, 'rb').read()
         image = MIMEImage(img_data)
-        image.add_header('Content-ID','<{}>'.format('SASM_LOGO_BLACK.png'))
+        image.add_header('Content-ID', '<{}>'.format('SASM_LOGO_BLACK.png'))
         msg.attach(image)
 
         msg.send()
-    
+
     def password_change(self, code: str, password: str):
         user_selector = UserSelector
         user = user_selector.get_user_from_code(code)
