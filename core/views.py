81fc2e46acb82c7e664cf689c9aa99cf2ba6837a
Yurim Park <63230463+lina1919@users.noreply.github.com>
2023-02-26 09:21:14 +0900

    #298 refactor : refactor user app code
    * #277 fix : "change sns data exception code"
    
    * refactor : refactor user app code
    
    * fix : enable me url

--- core/views.py
+++ core/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
-
+from rest_framework import status
+from rest_framework.response import Response
 # Create your views here.
+def get_paginated_response(*, pagination_class, serializer_class, queryset, request, view):
+    paginator = pagination_class()
+
+    page = paginator.paginate_queryset(queryset, request, view=view)
+
+    if page is not None:
+        serializer = serializer_class(page, many=True)
+    else:
+        serializer = serializer_class(queryset, many=True)
+
+    return Response({
+        'status': 'success',
+        'data': paginator.get_paginated_response(serializer.data).data,
+    }, status=status.HTTP_200_OK)
