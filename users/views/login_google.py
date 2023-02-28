08d631e27cf651a46831bea52c2feaaa946b2dc9
Sejong Kim <sepaper@naver.com>
2023-02-28 14:32:44 +0900

    #312 refactor : refactor google social login
    Signed-off-by: Sejong Kim <sepaper@naver.com>

--- users/views/login_google.py
+++ users/views/login_google.py
@@ -1,108 +1,78 @@
-from allauth.socialaccount.providers.google import views as google_view
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+
 from ..models import User
 from .social_login import *
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
+from rest_framework_simplejwt.tokens import RefreshToken
 
-BASE_URL = 'https://api.sasm.co.kr/'
-GOOGLE_CALLBACK_URI = 'https://www.sasm.co.kr/googleredirect/'
 
 @api_view(["GET", "POST"])
+@method_decorator(csrf_exempt)
 @permission_classes([AllowAny])
 def google_callback(request):
-    access_token = request.data.get('accessToken')
-    
-    email_req = requests.get(
-        f"https://www.googleapis.com/oauth2/v1/tokeninfo?access_token={access_token}"
+    access_token = request.GET.get('access_token')
+    user_info_req = requests.get(
+        f"https://www.googleapis.com/oauth2/v1/userinfo?access_token={access_token}"
     )
-    email_req_status = email_req.status_code
-    
-    if email_req_status != 200:
+
+    # 토큰을 이용해 사용자 정보를 가져오지 못했을 경우
+    if user_info_req.status_code != 200:
         return Response({
                         'status': 'error',
                         'message': 'failed to get email',
                         'code': 400
-                    }, status=status.HTTP_400_BAD_REQUEST)
-    email_req_json = email_req.json()
-    email = email_req_json.get('email')
-    nickname = email.split("@")[0]
-    
-    try:
-        user = User.objects.get(email=email)    
-        social_user = SocialAccount.objects.get(user=user)
-        if social_user is None:
-            return Response({
-                        'status': 'error',
-                        'message': 'Email exists but not social user',
-                        'code': 404
-                    }, status=status.HTTP_404_NOT_FOUND)
-        
-        if social_user.provider != 'google':
-            return Response({
-                        'status': 'error',
-                        'message': 'Social type does not match',
-                        'code': 404
-                    }, status=status.HTTP_404_NOT_FOUND)
-            
-        data = {'access_token': access_token}
-        accept = requests.post(
-            f"{BASE_URL}users/google/login/finish/", data=data
-        )
-        accept_status = accept.status_code
-        
-        if accept_status != 200:
-            return Response({
-                        'status': 'error',
-                        'message': 'Failed to signin',
-                        'code': accept_status
-                    }, status=accept_status)
-        accept_json = accept.json()
-        accept_json.pop('user', None)
-        
-        response = {
-                'access': accept_json.get('access_token'),
-                'refresh': accept_json.get('refresh_token'),
-                'nickname' : nickname
-            }
-        return Response({
-                    'status': 'success',
-                    'data': response,
-                }, status=status.HTTP_200_OK)
-        
-    except User.DoesNotExist:
-        data = {'access_token': access_token}
-        accept = requests.post(
-            f"{BASE_URL}users/google/login/finish/", data=data
-        )
-        accept_status = accept.status_code
-        
-        if accept_status != 200:
-            return Response({
-                        'status': 'error',
-                        'message': 'Failed to signup',
-                        'code': accept_status
-                    }, status=accept_status)
-            
-        user = User.objects.get(email=email)
-        user.is_active = True
-        user.nickname = nickname
-        user.save()
-            
-        accept_json = accept.json()
-        accept_json.pop('user', None)
-        
-        response = {
-                'access': accept_json.get('access_token'),
-                'refresh': accept_json.get('refresh_token'),
-                'nickname' : nickname
-            }
-        return Response({
-                    'status': 'success',
-                    'data': response,
-                }, status=status.HTTP_200_OK)
+                        }, status=status.HTTP_400_BAD_REQUEST)
 
-class GoogleLogin(SocialLoginView):
-    adapter_class = google_view.GoogleOAuth2Adapter
-    callback_url = GOOGLE_CALLBACK_URI
-    client_class = OAuth2Client
+    # 사용자 정보 추출
+    user_info = user_info_req.json()
+    email = user_info.get('email', None)
+    nickname = user_info.get('name', None)
+    # profile_image = user_info.get('picture', None)
+
+    # 필수 정보인 이메일과 닉네임을 성공적으로 가져오지 못한 경우 에러 반환
+    if not email or not nickname:
+        return Response({
+            'status': 'error',
+            'message': 'failed to get user email or nickname',
+            'code': 400
+        }, status=status.HTTP_400_BAD_REQUEST)
+
+    # 유저가 있는 경우 가져오고, 없는 경우 생성
+    if User.objects.filter(email=email, social_provider='google').exists():
+        user = User.objects.get(email=email, social_provider='google')
+    else:
+        # 해당 이메일이 다른 아이디에서 이미 사용 중인 경우
+        if User.objects.filter(email=email).exists():
+            return Response({
+                'status': 'error',
+                'message': 'email is already in use',
+                'code': 400
+            }, status=status.HTTP_400_BAD_REQUEST)
+
+        user = User(
+            email=email,
+            nickname=nickname,
+            is_active=True,
+            social_provider='google',
+            password=User.objects.make_random_password(20),
+        )
+
+        user.full_clean()
+        user.save()
+
+    # 로그인 수행
+    token = RefreshToken.for_user(user=user)
+    data = {
+        'email': user.email,
+        'refresh': str(token),
+        'access': str(token.access_token),
+        'nickname': user.nickname
+    }
+
+    return Response({
+        'status': 'success',
+        'data': data
+    }, status=status.HTTP_200_OK)
