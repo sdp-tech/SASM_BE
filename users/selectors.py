a643874d5207ee82dfc375ad40ac1d801e8abb07
Sejong Kim <sepaper@naver.com>
2023-04-03 00:14:22 +0900

    #310 fix : remove pw reuse check in pw reset
    Signed-off-by: Sejong Kim <sepaper@naver.com>

--- users/selectors.py
+++ users/selectors.py
@@ -6,6 +6,7 @@ from django.contrib.auth import authenticate
 from users.models import User
 from places.models import Place
 
+
 class UserSelector:
     def __init__(self):
         pass
@@ -17,7 +18,7 @@ class UserSelector:
     #         raise Http404
     #     except User.MultipleObjectsReturned:
     #         raise Http404
-    
+
     @staticmethod
     def get_user_from_email(email: str) -> User:
         try:
@@ -26,31 +27,26 @@ class UserSelector:
             raise Http404
         except User.MultipleObjectsReturned:
             raise Http404
-    
+
     @staticmethod
     def check_password(user: User, password: str):
         return user.check_password(password)
-    
+
     @staticmethod
     def check_email(email: str):
         return User.objects.filter(email=email).exists()
-    
+
     @staticmethod
     def check_nickname(nickname: str):
         return User.objects.filter(nickname=nickname).exists()
-    
+
     @staticmethod
     def get_user_like_place(user: User):
         return user.PlaceLikeUser.all()
-    
+
     @staticmethod
     def get_user_like_story(user: User):
         return user.StoryLikeUser.all()
-
-    @staticmethod
-    def check_password_exists(email: str, password: str):
-        if(authenticate(email=email, password=password) != None):
-            raise HttpResponseBadRequest
 
     @staticmethod
     def get_user_from_code(code: str) -> User:
@@ -68,14 +64,11 @@ class UserSelector:
             q.add(Q(category=category), q.OR)
 
         return queryset.filter(q)
-    
+
     @staticmethod
     def filter_story_by_query(filter: List, queryset: QuerySet) -> QuerySet:
         q = Q()
         for category in filter:
-            q.add(Q(address__category=category),q.OR)
+            q.add(Q(address__category=category), q.OR)
 
         return queryset.filter(q)
-
-
-
