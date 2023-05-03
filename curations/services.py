4393c041afa8c5216bb0f6948bebb6689a00bc7f
yew00n <111196056+yew00n@users.noreply.github.com>
2023-05-04 00:04:33 +0900

    #350 feat : set default value for CurationCreateApi (#371)
    * #350 feat : add place field in Story model
    
    * #350 feat : set default value in CurationCreateApi
    
    * #350 fix : allow curation photo not being updated

--- curations/services.py
+++ curations/services.py
@@ -20,13 +20,16 @@ class CurationCoordinatorService:
         self.user = user
 
     @transaction.atomic
-    def create(self, title: str, stories: list, short_curations: list, contents: str, rep_pic: InMemoryUploadedFile):
+    def create(self, title: str, stories: list, short_curations: list, contents: str, rep_pic: InMemoryUploadedFile, is_released: bool, is_selected: bool, is_rep: bool):
 
         curation_service = CurationService()
         curation = curation_service.create(
             title=title,
             contents=contents,
-            writer=self.user
+            writer=self.user,
+            is_released=is_released,  # 개발용
+            is_selected=is_selected,  # 개발용
+            is_rep=is_rep  # 개발용
         )
 
         short_curation_service = ShortCurationService()
@@ -67,9 +70,10 @@ class CurationCoordinatorService:
             stories=stories_qs
         )
 
-        photo_service = CurationPhotoService()
-        photo_service.update(
-            curation=curation, photo_image_url=photo_image_url, image_file=rep_pic)
+        if rep_pic:
+            photo_service = CurationPhotoService()
+            photo_service.update(
+                curation=curation, photo_image_url=photo_image_url, image_file=rep_pic)
 
         CurationMapService.delete(curation=curation)
         CurationMapService.create(curation=curation, stories=stories_qs)
@@ -146,11 +150,14 @@ class CurationService:
     def __init__(self):
         pass
 
-    def create(self, title: str, contents: str, writer: User):
+    def create(self, title: str, contents: str, writer: User, is_released: bool, is_selected: bool, is_rep: bool):
         curation = Curation(
             title=title,
             contents=contents,
-            writer=writer
+            writer=writer,
+            is_released=is_released,  # 개발용
+            is_selected=is_selected,  # 개발용
+            is_rep=is_rep  # 개발용
         )
 
         curation.full_clean()
