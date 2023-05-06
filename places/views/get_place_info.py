e3e7abf36ad17e22836e8e7b58f783c17570e69e
Sejong Kim <46182768+sejongk@users.noreply.github.com>
2023-05-06 16:02:25 +0900

    #376 feat : add place report api (#379)
    Signed-off-by: Sejong Kim <sepaper@naver.com>

--- places/views/get_place_info.py
+++ places/views/get_place_info.py
@@ -13,9 +13,9 @@ from drf_yasg import openapi
 
 from places.mixins import ApiAuthMixin
 from places.models import Place
-from places.serializers import PlaceSerializer,PlaceDetailSerializer
+from places.serializers import PlaceSerializer, PlaceDetailSerializer
 from places.selectors import PlaceSelector
-from sasmproject.swagger import param_search,param_filter,param_id
+from sasmproject.swagger import param_search, param_filter, param_id
 
 
 class MapMarkerApi(APIView, ApiAuthMixin):
@@ -33,7 +33,7 @@ class MapMarkerApi(APIView, ApiAuthMixin):
         responses={
             "200": openapi.Response(
                 description="OK",
-                examples = {
+                examples={
                     "application/json": {
                         'id': 1,
                         'place_name': '비건마마',
@@ -47,7 +47,6 @@ class MapMarkerApi(APIView, ApiAuthMixin):
             )
         }
     )
-
     def get(self, request):
         selector = PlaceSelector
         lat_lon = selector.lat_lon()
@@ -63,45 +62,46 @@ class BasicPagination(PageNumberPagination):
     page_size = 20
     page_size_query_param = 'page_size'
 
+
 class PlaceListView(viewsets.ModelViewSet):
     '''
         place의 list의 정보를 주는 API
     '''
     queryset = Place.objects.prefetch_related('place_likeuser_set')
     serializer_class = PlaceSerializer
-    permission_classes=[
+    permission_classes = [
         AllowAny,
     ]
-    pagination_class=BasicPagination
+    pagination_class = BasicPagination
 
     def filter_if_given(self, qs, query):
-        if(query):
+        if (query):
             qs = qs.filter(query)
         return qs
 
     def get_filter_query(self, array):
-        query = None
-        for a in array: 
-            if query is None: 
-                query = Q(category=a) 
-            else: 
+        query = Q(is_release=True)  # 심사 완료된 장소만 노출
+        for a in array:
+            if query is None:
+                query = Q(category=a)
+            else:
                 query = query | Q(category=a)
         return query
 
-    def search_if_given(self,search):
-        if(search):
+    def search_if_given(self, search):
+        if (search):
             qs = self.get_queryset().filter(Q(place_name__icontains=search))
         else:
             qs = self.get_queryset()
         return qs
 
     @swagger_auto_schema(operation_id='api_places_place_search_get',
-                        manual_parameters=[param_search,param_filter],security=[])
+                         manual_parameters=[param_search, param_filter], security=[])
     def get(self, request):
         '''
         search,filter를 적용한 장소 리스트를 distance로 정렬하여 반환
         '''
-        search = request.GET.get('search','')
+        search = request.GET.get('search', '')
         qs = self.search_if_given(search)
         array = request.query_params.getlist('filter[]', '배열')
         if array != '배열':
@@ -111,39 +111,41 @@ class PlaceListView(viewsets.ModelViewSet):
             qs,
             many=True,
             context={
-                "left":request.query_params.get("left"),
-                "right":request.query_params.get("right"),
-                "request":request
+                "left": request.query_params.get("left"),
+                "right": request.query_params.get("right"),
+                "request": request
             }
         )
-        
+
         # 검색 결과를 바탕으로 거리순 정렬 후 pagination
         serializer_data = sorted(
             serializer.data, key=lambda k: float(k['distance']))
         page = self.paginate_queryset(serializer_data)
-    
+
         if page is not None:
             serializer = self.get_paginated_response(page)
         else:
             serializer = self.get_serializer(page, many=True)
-            
+
         return Response({
             'status': 'success',
             'data': serializer.data,
         }, status=status.HTTP_200_OK)
-    
+
+
 class PlaceDetailView(viewsets.ModelViewSet):
     '''
         place의 detail 정보를 주는 API
     '''
     queryset = Place.objects.select_related('story')
     serializer_class = PlaceDetailSerializer
-    permission_classes=[
+    permission_classes = [
         AllowAny,
     ]
+
     @swagger_auto_schema(operation_id='api_places_place_detail_get',
-                        manual_parameters=[param_id],security=[])
-    def get(self,request):
+                         manual_parameters=[param_id], security=[])
+    def get(self, request):
         '''
             Place의 detail한 정보를 주는 api
         '''
@@ -151,5 +153,5 @@ class PlaceDetailView(viewsets.ModelViewSet):
         place = self.get_queryset().get(id=pk)
         return Response({
             'status': 'success',
-            'data': PlaceDetailSerializer(place,context={'request': request}).data,
+            'data': PlaceDetailSerializer(place, context={'request': request}).data,
         }, status=status.HTTP_200_OK)
---
+++ places/views/place_basic_views.py
@@ -0,0 +1,183 @@
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework import serializers
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.permissions import IsAuthenticated, AllowAny
+
+from rest_framework.views import APIView
+
+from places.models import Place
+from places.services import PlaceCoordinatorService
+from places.selectors import PlaceSnsTypeSelector
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
+
+
+class PlaceCreateApi(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    class PlaceCreateInputSerializer(serializers.Serializer):
+        place_name = serializers.CharField()
+        category = serializers.CharField()
+        vegan_category = serializers.CharField()
+        tumblur_category = serializers.BooleanField(required=False)
+        reusable_con_category = serializers.BooleanField(required=False)
+        pet_category = serializers.BooleanField(required=False)
+        mon_hours = serializers.CharField()
+        tues_hours = serializers.CharField()
+        wed_hours = serializers.CharField()
+        thurs_hours = serializers.CharField()
+        fri_hours = serializers.CharField()
+        sat_hours = serializers.CharField()
+        sun_hours = serializers.CharField()
+        etc_hours = serializers.CharField()
+        place_review = serializers.CharField()
+        address = serializers.CharField()
+        short_cur = serializers.CharField()
+        phone_num = serializers.CharField()
+        rep_pic = serializers.ImageField()
+        imageList = serializers.ListField()
+        snsList = serializers.ListField()
+
+        class Meta:
+            examples = {
+                'place_name': '안녕 상점',
+                'category': '식당 및 카페',
+                'vegan_category': '비건',
+                'tumblur_category': True,
+                'reusable_con_category': True,
+                'pet_category': True,
+                'mon_hours': '09:00 ~ 22:00',
+                'tues_hours': '09:00 ~ 22:00',
+                'wed_hours': '09:00 ~ 22:00',
+                'thurs_hours': '09:00 ~ 22:00',
+                'fri_hours': '09:00 ~ 22:00',
+                'sat_hours': '09:00 ~ 22:00',
+                'sun_hours': '09:00 ~ 22:00',
+                'etc_hours': '공휴일 09:00 ~ 22:00',
+                'place_review': '\"붉은 벽돌 외관에서의 담소\"',
+                'address': '서울 서대문구 연희로5길 22',
+                'short_cur': '연남장(場) 연희동 카페는 공간이 널찍하고 층고가 높습니다.',
+                'phone_num': '02-3141-7977',
+                'rep_pic': '< IMAGE FILE BINARY >',
+                'imageList': ['<IMAGE FILE BINARY>', '<IMAGE FILE BINARY>'],
+                'snsList': ['1,https://instagram.com/abc/', '2,https://www.sasm.co.kr/'],
+            }
+
+    @swagger_auto_schema(
+        request_body=PlaceCreateInputSerializer,
+        security=[],
+        operation_id='장소 제보하기',
+        operation_description='''
+            일반 유저를 위한 장소 제보하기 기능입니다.<br/>
+            vegan_category, tumblur_category, reusable_con_category의 경우, "알 수 없는 경우"에 해당하는 경우 false로 값을 넘겨주면 됩니다.<br/>
+            SNS 정보는 "SNS 타입 id(예: 1),https://instagram.com/abc/" 형태의 문자열로 snsList 필드에 담아보내면 됩니다. <br/>
+            SNS 타입 종류 리스트는 GET: /places/sns_types/을 통해서 가져올 수 있습니다.
+        ''',
+        responses={
+            "200": openapi.Response(
+                description="OK",
+                examples={
+                    "application/json": {
+                        "status": "success",
+                        "data": {"id": 1}
+                    }
+                }
+            ),
+            "400": openapi.Response(
+                description="Bad Request",
+            ),
+        },
+    )
+    def post(self, request):
+        serializer = self.PlaceCreateInputSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        data = serializer.validated_data
+
+        place = PlaceCoordinatorService.create(
+            place_name=data.get('place_name'),
+            category=data.get('category'),
+            vegan_category=data.get('vegan_category'),
+            tumblur_category=data.get('tumblur_category'),
+            reusable_con_category=data.get('reusable_con_category'),
+            pet_category=data.get('pet_category'),
+            mon_hours=data.get('mon_hours'),
+            tues_hours=data.get('tues_hours'),
+            wed_hours=data.get('wed_hours'),
+            thurs_hours=data.get('thurs_hours'),
+            fri_hours=data.get('fri_hours'),
+            sat_hours=data.get('sat_hours'),
+            sun_hours=data.get('sun_hours'),
+            etc_hours=data.get('etc_hours'),
+            place_review=data.get('place_review'),
+            address=data.get('address'),
+            short_cur=data.get('short_cur'),
+            phone_num=data.get('phone_num'),
+            rep_pic=data.get('rep_pic'),
+            imageList=data.get('imageList'),
+            snsList=data.get('snsList'),
+        )
+
+        return Response({
+            'status': 'success',
+            'data': {'id': place.id},
+        }, status=status.HTTP_201_CREATED)
+
+
+def get_paginated_response(*, pagination_class, serializer_class, queryset, request, view):
+    paginator = pagination_class()
+
+    page = paginator.paginate_queryset(queryset, request, view=view)
+
+    if page is not None:
+        serializer = serializer_class(page, many=True)
+    else:
+        serializer = serializer_class(queryset, many=True)
+
+    return Response({
+        'status': 'success',
+        'data': paginator.get_paginated_response(serializer.data).data,
+    }, status=status.HTTP_200_OK)
+
+
+class PlaceSnsTypeListApi(APIView):
+    permission_classes = (AllowAny,)
+
+    class Pagination(PageNumberPagination):
+        page_size = 10
+        page_size_query_param = 'page_size'
+
+    class PlaceSnsTypeListOutputSerializer(serializers.Serializer):
+        id = serializers.IntegerField()
+        name = serializers.CharField()
+
+    @swagger_auto_schema(
+        operation_id='장소 SNS 타입 리스트',
+        operation_description='''
+            사용 가능한 SNS 타입 리스트를 반환합니다.
+        ''',
+        responses={
+            "200": openapi.Response(
+                description="OK",
+                examples={
+                    "application/json": {
+                        'id': 1,
+                        'name': '인스타그램',
+                    }
+                }
+            ),
+            "400": openapi.Response(
+                description="Bad Request",
+            ),
+        },
+    )
+    def get(self, request):
+        sns_types = PlaceSnsTypeSelector.list()
+
+        return get_paginated_response(
+            pagination_class=self.Pagination,
+            serializer_class=self.PlaceSnsTypeListOutputSerializer,
+            queryset=sns_types,
+            request=request,
+            view=self
+        )
