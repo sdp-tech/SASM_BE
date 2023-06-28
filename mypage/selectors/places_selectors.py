3e09928b72899f3e86c9fa7e8e3c3c2bb2027edb
SeoJimin <jnuni004@naver.com>
2023-06-28 21:30:54 +0900

    #366 mypage/myplace/검색기능

---
+++ mypage/selectors/places_selectors.py
@@ -0,0 +1,52 @@
+from django.db.models import F, Q, Case, When, Value, Exists, OuterRef, CharField, BooleanField, Aggregate, Subquery
+from django.conf import settings
+from django.db.models.functions import Concat, Substr
+
+from users.models import User
+from places.models import PlaceVisitorReview, Place
+
+import traceback
+
+
+class UserReviewedPlaceSelector:
+    def __init__(self, user:User):
+        self.user = user
+    
+    def list(self):
+        user_reviews = PlaceVisitorReview.objects.filter(visitor_name__email=self.user)
+        reviewed_places = Place.objects.filter(id__in=user_reviews.values('place'))
+        
+        return reviewed_places
+    
+
+class MyPlaceSearchSelector:
+    def __init__(self, user: User):
+        self.user = user
+
+    def list(self, search: str = '', filter: list = []):
+        like_place = self.user.PlaceLikeUser.all()
+
+        q = Q()
+        q.add(Q(place_name__icontains=search) |
+              Q(category__icontains=search), q.AND)
+        
+        if len(filter) > 0:
+            query = None
+            for element in filter:
+                if query is None:
+                    query = Q(category=element)
+                else:
+                    query = query | Q(category=element)
+            q.add(query, q.AND)
+
+        # places = like_place.filter(q).annotate(
+        #     place_name = F('place_name'),
+        #     category=F('category'),
+        #     extra_pics=GroupConcat('photos__image'), 
+        # ).order_by('-created')
+
+        places = like_place.filter(q).order_by('-created')
+
+
+                
+        return places
---
+++ mypage/views/places_view.py
@@ -0,0 +1,176 @@
+from django.shortcuts import render, get_object_or_404
+from rest_framework.views import APIView
+from rest_framework import serializers, status
+from rest_framework.response import Response
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from core.views import get_paginated_response
+
+
+from places.models import PlaceVisitorReview, Place
+
+import users
+
+from mypage.selectors.places_selectors import UserReviewedPlaceSelector, MyPlaceSearchSelector
+
+from core.views import get_paginated_response
+
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
+
+#리뷰 작성한 장소 리스트룰 반환하는 API
+class UserReviewedPlaceGetApi(APIView):
+    class Pagination(PageNumberPagination):
+        page_size = 6
+        page_size_query_param = 'page_size'
+
+    class UserReviewedGetSerializer(serializers.ModelSerializer):
+
+        id = serializers.IntegerField()
+        place_name = serializers.CharField()
+        category = serializers.CharField()
+        rep_pic = serializers.ImageField()
+        address = serializers.CharField()
+
+
+        class Meta:
+            model = Place
+            fields = ['id', 'place_name','category','rep_pic','address']
+
+    @swagger_auto_schema(
+        operation_id='리뷰 작성한 장소 조회',
+        operation_description='''
+                유저가 리뷰 작성한 장소를 조회합니다.<br/>
+            ''',
+        responses={
+            "200": openapi.Response(
+                description="OK",
+                examples={
+                    "application/json": {
+                        "id": 1,
+                        "place_name": "타이거릴리",
+                        "category": "제로웨이스트 샵",
+                        "rep_pic": "https://sasm-bucket.s3.amazonaws.com/media/%ED%83%80%EC%9D%B4%EA%B1%B0%EB%A6%B4%EB%A6%AC.jpeg",
+                        "address": "서울 마포구 포은로 119 1층"
+                    }
+                }
+            ),
+            "400": openapi.Response(
+                description="Bad Request",
+            ),
+        },
+    )
+
+    def get(self,request):
+        self.user = request.user
+
+        selector = UserReviewedPlaceSelector(self.user)
+        places = selector.list()
+
+        serializer = self.UserReviewedGetSerializer(places, many=True)
+
+        return get_paginated_response(
+            pagination_class=self.Pagination,
+            serializer_class=self.UserReviewedGetSerializer,
+            queryset=places,
+            request=request,
+            view=self
+        )
+    
+
+#좋아요한 장소에서 검색하는 API
+class MyPlaceSearchApi(APIView):
+
+    permission_classes = (IsAuthenticated, )
+
+    class Pagination(PageNumberPagination):
+        page_size = 6
+        page_size_query_param = 'page_size'
+
+    class MyPlaceListFilterSerializer(serializers.Serializer):
+        search = serializers.CharField(required=False)
+        # order = serializers.CharField(required=False)
+        filter = serializers.ListField(required=False)
+    
+    class MyPlaceListOutputSerializer(serializers.Serializer):
+        id=serializers.IntegerField()
+        place_name = serializers.CharField()
+        category = serializers.CharField()
+        rep_pic = serializers.ImageField()
+        address = serializers.CharField()
+        # created = serializers.DateTimeField()
+
+        def get_place_name(self, obj):
+            return obj.place.place_name
+        
+        def get_place_like(self, obj):
+            re_user = self.context['request'].user
+            likes = users.selector.get_user_like_place(re_user)
+            return likes
+    
+    @swagger_auto_schema(
+        operation_id='마이 플레이스 서치',
+        operation_description='''
+            전달된 쿼리 파라미터에 부합하는 장소 리스트를 반환합니다.<br/>
+            ''',
+        query_serializer=MyPlaceListFilterSerializer,
+        responses={
+            "200": openapi.Response(
+                description="OK",
+                examples={
+                    "application/json": {
+                        'id': 1,
+                        'place_name': '서울숲',
+                        'category': '녹색 공간',
+                        'rep_pic': 'https://abc.com/1.jpg',
+                        'created': '2023-08-24T14:15:22Z',
+                        'address' : '서울 마포구 포은로 119 1층',
+                    }
+                }
+            ),
+            "400": openapi.Response(
+                description="Bad Request",
+            ),
+        },
+    )
+    def get(self, request):
+        filters_serializer = self.MyPlaceListFilterSerializer(
+            data = request.query_params)
+        filters_serializer.is_valid(raise_exception=True)
+        filters = filters_serializer.validated_data
+
+        selector = MyPlaceSearchSelector(user = request.user)
+        like_place = selector.list(
+            search = filters.get('search', ''),
+            filter = filters.get('filter', []),
+        )
+
+        return get_paginated_response(
+            pagination_class=self.Pagination,
+            serializer_class=self.MyPlaceListOutputSerializer,
+            queryset=like_place,
+            request=request,
+            view=self,
+        )
+
+        # places = MyPlaceSearchSelector. list(
+        #   search = filters.get('search', ''),
+        #   order = filters.get('order','latest')
+        # )
+
+        # serializer = self.MyPlaceListOutputSerializer(
+        #     places, many = True
+        # )
+
+        # return Response({
+        #     'status': 'success',
+        #     'data' : serializer.data,
+        # }, status=status.HTTP_200_OK)
+    
+        # return get_paginated_response(
+        #     pagination_class=self.Pagination,
+        #     serializer_class=self.MyPlaceListFilterSerializer,
+        #     queryset=place,
+        #     request=request,
+        #     view=self
+        # )
