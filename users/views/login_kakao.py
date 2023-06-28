90698ed634d00a874925a687bcb9ffe524b7ae9c
Sejong Kim <sepaper@naver.com>
2023-06-29 01:31:29 +0900

    #440 feat : change social login api for mobile app
    Signed-off-by: Sejong Kim <sepaper@naver.com>

--- users/views/login_kakao.py
+++ users/views/login_kakao.py
@@ -14,15 +14,18 @@ from core.exceptions import ApplicationError
 @method_decorator(csrf_exempt)
 @permission_classes([AllowAny])
 def kakao_callback(request):
-    rest_api_key = getattr(settings, 'KAKAO_REST_API_KEY')
-    code = request.GET.get("code")
-    redirect_uri = 'https://www.sasm.co.kr/auth/kakao/callback/'
+    if 'access_token' not in request.GET:
+        rest_api_key = getattr(settings, 'KAKAO_REST_API_KEY')
+        code = request.GET.get("code")
+        redirect_uri = 'https://www.sasm.co.kr/auth/kakao/callback/'
 
-    # 인가 코드를 이용해 사용자 정보에 접근할 수 있는 엑세스 토큰 받아오기
-    access_token_res = requests.get(
-        f"https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id={rest_api_key}&redirect_uri={redirect_uri}&code={code}"
-    )
-    access_token = access_token_res.json().get("access_token")
+        # 인가 코드를 이용해 사용자 정보에 접근할 수 있는 엑세스 토큰 받아오기
+        access_token_res = requests.get(
+            f"https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id={rest_api_key}&redirect_uri={redirect_uri}&code={code}"
+        )
+        access_token = access_token_res.json().get("access_token")
+    else:
+        access_token = request.GET.get('access_token')
 
     # 받아온 엑세스 토큰으로 사용자 정보 가져오기
     profile_res = requests.get(
--- users/views/login_naver.py
+++ users/views/login_naver.py
@@ -24,34 +24,38 @@ for i in range(STATE_LENGTH):
 @method_decorator(csrf_exempt)
 @permission_classes([AllowAny])
 def naver_callback(request):
-    client_id = getattr(settings, 'NAVER_CLIENT_ID')
-    client_secret = getattr(settings, "NAVER_SECRET_KEY")
+    if 'access_token' not in request.GET:
+        client_id = getattr(settings, 'NAVER_CLIENT_ID')
+        client_secret = getattr(settings, "NAVER_SECRET_KEY")
 
-    # 유저가 권한 승인하지 않은 경우
-    if 'code' not in request.GET:
-        return Response({
-            'status': 'error',
-            'message': 'user canceled the authorization process',
-            'code': 400
-        }, status=status.HTTP_400_BAD_REQUEST)
+        # 유저가 권한 승인하지 않은 경우
+        if 'code' not in request.GET:
+            return Response({
+                'status': 'error',
+                'message': 'user canceled the authorization process',
+                'code': 400
+            }, status=status.HTTP_400_BAD_REQUEST)
 
-    # 유저가 권한 승인한 경우
-    code = request.GET.get('code')
+        # 유저가 권한 승인한 경우
+        code = request.GET.get('code')
 
-    # 네이버 계정 정보를 가져오기 위한 액세스 토큰 요청
-    provider_token_response = requests.get(
-        f"https://nid.naver.com/oauth2.0/token?grant_type=authorization_code&client_id={client_id}&client_secret={client_secret}&code={code}&state={state}"
-    ).json()
+        # 네이버 계정 정보를 가져오기 위한 액세스 토큰 요청
+        provider_token_response = requests.get(
+            f"https://nid.naver.com/oauth2.0/token?grant_type=authorization_code&client_id={client_id}&client_secret={client_secret}&code={code}&state={state}"
+        ).json()
 
-    if 'error' in provider_token_response:
-        return Response({
-            'status': 'error',
-            'message': 'failed to get user access token from provider',
-            'code': 400
-        }, status=status.HTTP_400_BAD_REQUEST)
+        if 'error' in provider_token_response:
+            return Response({
+                'status': 'error',
+                'message': 'failed to get user access token from provider',
+                'code': 400
+            }, status=status.HTTP_400_BAD_REQUEST)
+
+        access_token = provider_token_response.get("access_token")
+    else:
+        access_token = request.GET.get('access_token')
 
     # 액세스 토큰을 이용해 유저 정보 가져오기
-    access_token = provider_token_response.get("access_token")
     user_info_response = requests.get(
         "https://openapi.naver.com/v1/nid/me",
         headers={"Authorization": f"Bearer {access_token}"}).json().get('response')
