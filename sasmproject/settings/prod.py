4e62d2f35b293aa6e0e76b9a3c690c5a1e0a1d36
Sejong Kim <sepaper@naver.com>
2023-07-03 01:04:45 +0900

    chore : remove useless setup in prod settings
    Signed-off-by: Sejong Kim <sepaper@naver.com>

--- sasmproject/settings/prod.py
+++ sasmproject/settings/prod.py
@@ -1,6 +1,4 @@
 from .base import *
-import sentry_sdk
-from sentry_sdk.integrations.django import DjangoIntegration
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
@@ -14,25 +12,6 @@ DATABASES = {
         "HOST": env('SASM_BE_DB_HOST'),
         "PORT": env('SASM_BE_DB_PORT'),
     }
-}
-
-sentry_sdk.init(
-    dsn="https://404ba4a0e77d437e8755decbc545c8eb@o4504016282583040.ingest.sentry.io/4504016285270016",
-    integrations=[DjangoIntegration()],
-    traces_sample_rate=1.0,
-
-    # If you wish to associate users to errors (assuming you are using
-    # django.contrib.auth) you may enable sending PII data.
-    send_default_pii=True
-)
-
-REST_FRAMEWORK = {
-    "DEFAULT_PERMISSION_CLASSES": ["rest_framework.permissions.IsAuthenticated", ],
-    "DEFAULT_AUTHENTICATION_CLASSES": [
-        "rest_framework_simplejwt.authentication.JWTAuthentication",
-        "rest_framework.authentication.TokenAuthentication",
-        "rest_framework.authentication.SessionAuthentication",
-    ],
 }
 
 LOGGING['loggers']['django']['handlers'].append('file')
