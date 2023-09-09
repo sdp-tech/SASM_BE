1da0adedfa800844d4abedc9f77f4e54032c5304
student31 <theyummy@khu.ac.kr>
2023-09-09 15:34:44 +0900

    #518 feat : add other_curation and other_forest

--- mypage/views/forest_views.py
+++ mypage/views/forest_views.py
@@ -7,6 +7,7 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.exceptions import NotFound, ValidationError
 
 from users.mixins import ApiAuthMixin,  ApiAllowAnyMixin
 from mypage.selectors.curations_selectors import CurationSelector
@@ -102,7 +103,99 @@ class UserForestListApi(APIView):
             request=request,
             view=self,
         )
-    
+
+
+class OtherUserCreateForestApi(APIView):
+    permission_classes = (IsAuthenticated, )
+
+    class Pagination(PageNumberPagination):
+        page_size = 4
+        page_size_query_param = 'page_size'
+
+    class OtherUserCreatedForestFilterSerializer(serializers.Serializer):
+        search = serializers.CharField(required=False)
+        category_filter = serializers.ListField(required=False)
+
+    class OtherUserCreatedForestOutputSerializer(serializers.Serializer):
+        id = serializers.IntegerField()
+        title = serializers.CharField()
+        preview = serializers.SerializerMethodField()
+        forest_like = serializers.SerializerMethodField()
+        rep_pic = serializers.ImageField()
+        writer = serializers.CharField()
+        nickname = serializers.CharField()
+        writer_is_verified = serializers.BooleanField()
+
+        def get_preview(self, obj):
+            forest = get_object_or_404(Forest, id=obj.id)
+            ret = re.sub(r'<img.*?>', ' ', forest.content)
+            ret = re.sub(r'<.*?>', '', ret)  # FYI: 닫는 태그 <\/.+?>
+            ret = re.sub(r'\s{2,}', ' ', ret)  # space 두개 이상인 경우 하나로
+            return ret[:100]
+
+        def get_forest_like(self, obj):
+            re_user = self.context['request'].user
+            likes = ForestSelector.likes(obj, user=re_user)
+            return likes
+
+    @swagger_auto_schema(
+        query_serializer=OtherUserCreatedForestFilterSerializer,
+        operation_id='마이페이지 내가 작성한 포레스트 리스트',
+        operation_description='''
+            내가 작성한 포레스트 게시글을 불러옵니다.
+        ''',
+        responses={
+            "200": openapi.Response(
+                description="OK",
+                examples={
+                    "application/json": {
+                        'id': 1,
+                        'title': "신재생 에너지 종류",
+                        'preview': '풍력발전이란? 풍력발전은 바람과 ...',
+                        'forest_likes': True,
+                        'rep_pic': 'https://abc.com/1.jpg',
+                        'writer': 'sdpofficial',
+                        'nickname': 'sdpygl',
+                        'writer_is_verified': True,
+                    }
+                }
+            ),
+            "400": openapi.Response(
+                description="Bad Request",
+            ),
+        },
+    )
+    def get(self, request):
+        filters_serializer = self.OtherUserCreatedForestFilterSerializer(
+            data=request.query_params)
+        filters_serializer.is_valid(raise_exception=True)
+        filters = filters_serializer.validated_data
+        
+        target_email = request.query_params.get('email')
+            
+        if not target_email:
+            raise ValidationError(detail="이메일 쿼리 파라메터 필요")
+                
+        try:
+            user = User.objects.get(email=target_email)
+        except User.DoesNotExist:
+            raise NotFound(detail="유저를 찾을 수 없음")
+
+
+        selector = UserCreatedForestSelector(user=user)
+        user_forest = selector.list(
+            search=filters.get('search', ''),
+            category_filter=filters.get('category_filter', []),
+        )
+
+        return get_paginated_response(
+            pagination_class=self.Pagination,
+            serializer_class=self.OtherUserCreatedForestOutputSerializer,
+            queryset=user_forest,
+            request=request,
+            view=self,
+        )
+
 
 class UserCreateForestApi(APIView):
     permission_classes = (IsWriter, )
