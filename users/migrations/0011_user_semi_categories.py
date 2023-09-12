68a97d19eb835236f7f4c2b5e868ce24500983eb
Jiae Kim <82318174+KJA000@users.noreply.github.com>
2023-09-12 22:34:15 +0900

    fix : modify user migration (#522)

--- users/migrations/0011_user_semi_categories.py
+++ users/migrations/0011_user_semi_categories.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('forest', '0007_delete_forestusercategory'),
+        # ('forest', '0007_delete_forestusercategory'),
         ('users', '0010_user_introduction'),
     ]
 
