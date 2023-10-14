e5102d7b2e049429746b38c699f3eca555f80009
Jiae Kim <82318174+KJA000@users.noreply.github.com>
2023-10-14 14:10:19 +0900

    #538 feat : add OtherUserReviewedPlaceGetApi and other_reviewd_place url (#539)
    * #535 feat : add story_id field to place_detail
    
    * #535 feat : add story_id field to place_detail
    
    * #535 feat : add story_id field to place_detail
    
    * #538 feat : add OtherUserReviewedPlaceGetApi and other_reviewd_place url

--- mypage/views/places_view.py
+++ mypage/views/places_view.py
@@ -4,8 +4,11 @@ from rest_framework import serializers, status
 from rest_framework.response import Response
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.exceptions import NotFound, ValidationError
+
 from core.views import get_paginated_response
 
+from users.models import User
 
 from places.models import PlaceVisitorReview, Place
 
@@ -77,6 +80,71 @@ class UserReviewedPlaceGetApi(APIView):
             view=self
         )
     
+
+class OtherUserReviewedPlaceGetApi(APIView):
+    class Pagination(PageNumberPagination):
+        page_size = 6
+        page_size_query_param = 'page_size'
+
+    class OtherUserReviewedGetSerializer(serializers.ModelSerializer):
+
+        id = serializers.IntegerField()
+        place_name = serializers.CharField()
+        category = serializers.CharField()
+        rep_pic = serializers.ImageField()
+        address = serializers.CharField()
+
+
+        class Meta:
+            model = Place
+            fields = ['id', 'place_name','category','rep_pic','address']
+
+    @swagger_auto_schema(
+        operation_id='리뷰 작성한 장소 조회',
+        operation_description='''
+                유저가 리뷰 작성한 장소를 조회합니다. 쿼리 파라메터로 'email' 필요(타겟 유저의 이메일)<br/>
+            ''',
+        responses={
+            "200": openapi.Response(
+                description="OK",
+                examples={
+                    "application/json": {
+                        "id": 1,
+                        "place_name": "타이거릴리",
+                        "category": "제로웨이스트 샵",
+                        "rep_pic": "https://sasm-bucket.s3.amazonaws.com/media/%ED%83%80%EC%9D%B4%EA%B1%B0%EB%A6%B4%EB%A6%AC.jpeg",
+                        "address": "서울 마포구 포은로 119 1층"
+                    }
+                }
+            ),
+            "400": openapi.Response(
+                description="Bad Request",
+            ),
+        },
+    )
+
+    def get(self,request):
+        target_email = request.query_params.get('email')
+            
+        if not target_email:
+            raise ValidationError(detail="이메일 쿼리 파라메터 필요")
+                
+        try:
+            self.user = User.objects.get(email=target_email)
+        except User.DoesNotExist:
+            raise NotFound(detail="유저를 찾을 수 없음")
+
+        selector = UserReviewedPlaceSelector(self.user)
+        places = selector.list()
+
+        return get_paginated_response(
+            pagination_class=self.Pagination,
+            serializer_class=self.OtherUserReviewedGetSerializer,
+            queryset=places,
+            request=request,
+            view=self
+        )
+
 
 #좋아요한 장소에서 검색하는 API
 class MyPlaceSearchApi(APIView):
