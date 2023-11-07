323c5f41741e17adae3c8581513b2758644af1b3
SeoJimin1234 <113419021+SeoJimin1234@users.noreply.github.com>
2023-11-07 15:19:41 +0900

    feat:Total Search for Curation, Forest, Story (#544)
    * #540 fix: Curation Order set default
    
    * feat: Total Search for Curation,Forest,Story
    
    ---------
    
    Co-authored-by: SeoJimin <jnuni004@naver.com>

--- curations/selectors.py
+++ curations/selectors.py
@@ -4,12 +4,14 @@ from datetime import datetime
 from curations.models import Curation
 from users.models import User
 from stories.models import Story
+from forest.models import Forest
 
 from django.conf import settings
 from django.db.models.functions import Concat, Substr
 from django.db.models import Q, F, Case, When, Value, Exists, OuterRef, CharField, BooleanField, Aggregate, Count
 from dataclasses import dataclass
 
+from itertools import chain
 
 class GroupConcat(Aggregate):
     # Postgres ArrayAgg similar(not exactly equivalent) for sqlite & mysql
@@ -285,3 +287,150 @@ class CurationLikeSelector:
             return True
         else:
             return False
+        
+
+class TotalSearchSelector:
+    def __init__(self):
+        pass
+    
+    @staticmethod
+    def list(search: str='', order: str='', user: User=None):
+        
+        order_by_time = {'latest': '-created', 'oldest': 'created'}
+
+        if order in order_by_time:
+            order = order_by_time[order]
+        else:
+            order = 'created'
+
+        #curation에 대한 검색을 하고 결과를 curations에 저장
+        q_curation = Q()
+        
+        q_curation.add(Q(title__icontains=search) |
+                       Q(contents__icontains=search) |
+                       Q(story__title__icontains=search) |
+                       Q(story__place__place_name__icontains=search) |
+                       Q(story__place__category__icontains=search) |
+                       Q(story__tag__icontains=search), q_curation.AND)
+
+        curations = Curation.objects.distinct().filter(q_curation, is_released=True).annotate(
+            user_likes=Case(
+                When(Exists(Curation.likeuser_set.through.objects.filter(
+                    curation_id=OuterRef('pk'),
+                    user_id=user.pk
+                )),
+                    then=Value(1)),
+                default=Value(0),
+            ),
+            like_cnt=Count('likeuser_set'),
+            is_followed = Exists(
+            user.follows.through.objects.filter(
+                from_user_id=user.id,
+                to_user_id=OuterRef('writer_id')
+                )
+            ) if user.is_authenticated else Value(False),
+            rep_pic=Case(
+                When(
+                    photos__image=None,
+                    then=None
+                ),
+                default=Concat(Value(settings.MEDIA_URL),
+                               F('photos__image'),
+                               output_field=CharField())
+            ),
+            nickname = F('writer__nickname'),
+        ).order_by(order)
+
+        #orest에 대한 검색을 하고 결과를 forests에 저장
+        q_forest = Q()
+        q_forest.add(Q(title__icontains=search) |
+              Q(subtitle__icontains=search) |
+              Q(content__icontains=search) |
+              #   Q(category__name__icontains=search) |
+              #   Q(semi_categories__name__icontains=search) |
+              Q(hashtags__name__icontains=search), q_forest.AND)
+        
+        forests = Forest.objects.distinct().annotate(
+            user_likes=Case(
+                When(Exists(Forest.likeuser_set.through.objects.filter(
+                    forest_id=OuterRef('pk'),
+                    user_id=user.pk
+                )),
+                    then=Value(1)),
+                default=Value(0),
+            ),
+            is_followed = Exists(
+            user.follows.through.objects.filter(
+                from_user_id=user.id,
+                to_user_id=OuterRef('writer_id')
+                )
+            ) if user.is_authenticated else Value(False),
+            nickname=F('writer__nickname'),
+        ).filter(q_forest).order_by(order)
+
+        #story에 대한 검색을하고 결과를 stories에 저장
+        q_story=Q()
+        q_story.add(Q(title__icontains=search) |
+              Q(place__place_name__icontains=search) |  # 스토리 제목 또는 내용 검색
+              Q(place__category__icontains=search) |
+              Q(tag__icontains=search), q_story.AND)
+        
+        stories = Story.objects.filter(q_story).annotate(
+            user_likes=Case(
+                When(Exists(Story.story_likeuser_set.through.objects.filter(
+                    story_id=OuterRef('pk'),
+                    user_id=user.pk
+                )),
+                    then=Value(1)),
+                default=Value(0),
+            ),
+            is_followed = Exists(
+            user.follows.through.objects.filter(
+                from_user_id=user.id,
+                to_user_id=OuterRef('writer_id')
+                )
+            ) if user.is_authenticated else Value(False),
+            nickname=F('writer__nickname'),
+        ).distinct().order_by(order)
+
+        #curations,forests,stories 쿼리셋에서 데이터 추출하고 변환
+        curations_data = [{'id': c.id, 
+                           'model':c.__class__.__name__, 
+                           'title':c.title, 
+                           'content': c.contents,
+                           'like_cnt':c.like_cnt,
+                           'writer_is_followed':c.is_followed,
+                           'created':c.created,
+                           'rep_pic':c.rep_pic,
+                           'nickname':c.nickname,
+                           'user_likes': c.user_likes,
+                           } for c in curations]
+        
+        forests_data = [{'id': f.id, 
+                         'model':f.__class__.__name__, 
+                         'title':f.title, 
+                         'content':f.subtitle,
+                         'like_cnt':f.like_cnt,
+                         'writer_is_followed':f.is_followed,
+                         'created':f.created.strftime("%Y-%m-%d %H:%M:%S"),
+                         'rep_pic':f.rep_pic.url,
+                         'nickname':f.nickname,
+                         'user_likes':f.user_likes,
+                         } for f in forests]
+        
+        stories_data = [{'id': s.id, 
+                         'model':s.__class__.__name__,
+                         'title':s.title, 
+                         'content':s.preview,
+                         'like_cnt':s.story_like_cnt,
+                         'writer_is_followed':s.is_followed,
+                         'created':s.created.strftime("%Y-%m-%d %H:%M:%S"),
+                         'rep_pic':s.rep_pic.url,
+                         'nickname':s.nickname,
+                         'user_likes':s.user_likes,
+                         } for s in stories]
+
+        #세개의 데이터를 체인으로 연결
+        result = list(chain(curations_data, forests_data, stories_data))
+
+        return result
